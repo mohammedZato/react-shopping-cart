e08dac6a964173ea5d554cc187ae98eb91f1c296
mohammed zato <zatomohammed231@gmail.com>
2025-01-21 19:26:54 +0000

    updated files

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        explore: 'src/Pages/Explore.jsx',
-        productDetails: 'src/Pages/ProductDetails.jsx',
-        cartItems: 'src/Pages/CartItems.jsx',
-      },
-    },
-  },
+  // base: '/my-react-shopping-cart-app/',
 })
